c5fba500d4b4723bad84ee06e7678939c7f4ec35
Duico <a.duico@student.tudelft.nl>
2022-10-27 16:34:11 +0200

    Make the script work with the given image directory and update readme

--- nuscenes_mask_generation/README.md
+++ nuscenes_mask_generation/README.md
@@ -3,8 +3,6 @@
 1. Install detectron2 by following the instructions here: https://detectron2.readthedocs.io/en/latest/tutorials/install.html
 You need to make sure that you have CUDA, torch and torchvision installed, as well as the correct python version configured. Afterwards, you can simply run this to install detectron2: `python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'`
 
-2. Install nuscenes-devkit: `pip install nuscenes-devkit`
+2. Install the progress bar: `pip install tqdm`
 
-3. Install the progress bar: `pip install tqdm`
-
-4. Run the script by passing the nuScenes root directory and the scene index of your interest. The script will create a `masks/` folder with the masks for every single photo of the driving sequence (including all cameras).
+3. Run the script by passing the image directory. The script will create the masks inside the given directory for each image.
--- nuscenes_mask_generation/create_masks.py
+++ nuscenes_mask_generation/create_masks.py
@@ -1,18 +1,14 @@
-from unicodedata import name
-import torch
-
 # Setup detectron2 logger
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
 # import some common libraries
 import numpy as np
+import torch
 import os
-import os.path as osp
 import cv2
 import sys
 from tqdm import tqdm
-from PIL import Image
 
 # import some common detectron2 utilities
 from detectron2 import model_zoo
@@ -21,16 +17,10 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 
-# import nuScenes-devkit
-from nuscenes.nuscenes import NuScenes
-
 # IDs for the following classes of interest: ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "bird", "cat", "dog"]
-# Essentially, we only want to keep moving entities that may occur in nuscenes images
+# Essentially, we only want to keep moving entities that may occur in the images
 # see https://github.com/facebookresearch/detectron2/issues/147#issuecomment-645958806
 CLASSES_OF_INTEREST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 14, 15, 16]
-
-# Define nuScenes camera channels
-NUSCENES_CAMERA_CHANNELS = ["CAM_FRONT", "CAM_FRONT_LEFT", "CAM_FRONT_RIGHT", "CAM_BACK_LEFT", "CAM_BACK_RIGHT", "CAM_BACK"]
 
 # Get Model's Config and instantiate the predictor
 cfg = get_cfg()
@@ -59,46 +49,24 @@ def create_mask(image, mask_filename):
     mask_file = output.get_image()[:, :, ::-1]
     cv2.imwrite(mask_filename, mask_file)
 
-def process_nuscenes_record(nusc, record):
-    for camera_channel in NUSCENES_CAMERA_CHANNELS:
-        camera_token = record['data'][camera_channel]
-        cam = nusc.get('sample_data', camera_token)
-        filename = cam['filename'].split("/")[2][:-4]
-        im = cv2.imread(osp.join(nusc.dataroot, cam['filename']))
-        mask_filename = f'masks/dynamic_mask_{filename}.png'
+def create_masks(image_dir):
+    print("Start creating masks for the specified images...")
+
+    filenames = os.listdir(image_dir)
+    pbar = tqdm(total=len(filenames))
+
+    for filename in filenames:
+        im = cv2.imread(os.path.join(image_dir, filename))
+        filename_without_extension = filename[:-4]
+        mask_filename = f'{image_dir}/dynamic_mask_{filename_without_extension}.png'
         create_mask(im, mask_filename)
-
-def create_masks_for_nuscenes_sequence(nuscenes_root_dir, scene_index):
-    # Process nuscenes images
-    nusc = NuScenes(version='v1.0-mini', dataroot=nuscenes_root_dir, verbose=True)
-    scene = nusc.scene[scene_index]
-    current_token = scene["first_sample_token"]
-    last_sample_token = scene["last_sample_token"]
-    n_samples = scene["nbr_samples"]
-
-    print("Start creating masks for nuscenes images...")
-
-    pbar = tqdm(total=n_samples)
-    while current_token != last_sample_token:
-        sample_record = nusc.get('sample', current_token)
-        process_nuscenes_record(nusc, sample_record)
-        current_token = sample_record["next"]
         pbar.update(1)
-    pbar.close()
 
-    last_record = nusc.get('sample', last_sample_token)
-    process_nuscenes_record(nusc, last_record)
+    pbar.close()
 
     print("Masks successfully created!")
 
 if __name__ == "__main__":
-    # Create mask directories
-    masks_path = "masks"
-    if not osp.exists(masks_path):
-        os.makedirs(masks_path)
-
-    nuscenes_root_dir = sys.argv[1]
-    nuscenes_scene_index = int(sys.argv[2])
-
     # Create and save masks
-    create_masks_for_nuscenes_sequence(nuscenes_root_dir, nuscenes_scene_index)
+    image_dir = sys.argv[1]
+    create_masks(image_dir)
